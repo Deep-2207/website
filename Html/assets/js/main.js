860a04cae368d6050d6f29556bde954acd561854
Deep-2207 <pdeep4679@gmail.com>
2021-11-27 11:18:52 +0530

    home page

---
+++ Html/assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".homePage.navbar").addClass("navbar-bg-color");
+    } else {
+      $(".homePage.navbar").removeClass("navbar-bg-color");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  $(".top-arrow").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  var posts = $(".filter-images");
+  posts.hide();
+
+  $(".container-max-width .control .buttons").click(function () {
+    var customType = $(this).data("filter");
+    console.log(customType);
+    console.log(posts.length);
+
+    posts.hide();
+    $("." + customType).show();
+  });
+});
