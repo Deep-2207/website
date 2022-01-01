7ff895d0a08cb80df1d17b9ce416707ed423b31b
Deep-2207 <pdeep4679@gmail.com>
2022-01-01 23:31:23 +0530

    Completed Upcoming service, customer-service-provider and Working on admin-usermanagement Page

--- Html/assets/js/main.js
+++ Html/assets/js/main.js
@@ -27,4 +27,47 @@ $(document).ready(function () {
   //   posts.hide();
   //   $("." + customType).show();
   // });
+  
+  var items = document.querySelectorAll("#sidebar-menu li"),
+   tab =[],index;
+   
+   for(var i=0;i<items.length;i++){
+     tab.push(items[i].innerHTML);
+     console.log(tab[i]);
+   } 
+  for(var i=0;i<items.length;i++){
+    items[i].onclick = function(){
+      index = tab.indexOf(this.innerHTML);
+      console.log(index + "COnsole check");
+    };
+  }
+// data table js start
+//   $(document).ready(function() {
+//     $('#example').DataTable( {
+//         "order": [[ 3, "desc" ]],
+//         "bPaginate": false, //hide pagination
+//         "bFilter": false, //hide Search bar
+//         "bInfo": false, // hide showing entries
+        
+//     } );
+// } );
+
+
+
+          // $('#example').dataTable({
+          //     "bProcessing": true,
+          //     "sAutoWidth": false,
+          //     "bDestroy":true,
+          //     "sPaginationType": "bootstrap", // full_numbers
+          //     "iDisplayStart ": 10,
+          //     "iDisplayLength": 10,
+          //"bPaginate": false, //hide pagination
+          //   "bFilter": false, //hide Search bar
+          //     "bInfo": false, // hide showing entries
+          // })
+          //    
+// data table js end
+
+
+
 });
